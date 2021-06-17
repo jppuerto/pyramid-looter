fae261ae9b7be9e3c312c7b8a9671bf0fc501470
valentinvisintin <whitex@hotmail.fr>
2021-06-17 12:58:09 +0200

    Mise à jour tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -13,6 +13,8 @@ namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const tile5 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
     export const tile6 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
@@ -94,7 +96,7 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 `, [myTiles.transparency16], TileScale.Sixteen);
             case "labyrinthe":
-            case "level3":return tiles.createTilemap(hex`1000100001010101010101010101010101010101010303030303030403040303040403030102040303040404030404020404030101030404040403030303040303030301010303030303030404030403040404010103040404040304040303030304040101030403030303030303040403030301010304030404040304040404030404010103040304030303030304040304030101030402040304040403040402040301010304030403030304020404030403010104040304040403040304040304030101030303030304030403030303030301010304040404040304040403040404010103030303030303030303030303030101030101010101010101010101010101`, img`
+            case "level3":return tiles.createTilemap(hex`1000100001010101010101010101010101010101010303030303030405040303040403030102040303040404030404020404030101030404040403030303040303030301010303030303030404030403040404010103040404040304040303030304040101030405030303030303040403030301010304030404040304040404030404010103040304030303030304040304050101030402040304040403040402040301010504030403030304020404030403010104040304040403040304040304030101030303030304030403030303030301010304040404040304040403040404010103030303030303030303030303030101030101010101010101010101010101`, img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -111,7 +113,7 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
-`, [myTiles.transparency16,myTiles.tile4,myTiles.tile6,sprites.dungeon.darkGroundCenter,sprites.dungeon.floorLight4], TileScale.Sixteen);
+`, [myTiles.transparency16,myTiles.tile4,myTiles.tile6,sprites.dungeon.darkGroundCenter,sprites.dungeon.floorLight4,myTiles.tile7], TileScale.Sixteen);
         }
         return null;
     })
@@ -129,6 +131,8 @@ namespace myTiles {
             case "tile3":return tile3;
             case "eau":
             case "tile5":return tile5;
+            case "myTile3":
+            case "tile7":return tile7;
             case "myTile2":
             case "tile6":return tile6;
         }
