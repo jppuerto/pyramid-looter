e3b20310c74fe5b60084aa9fba478e3688b3348a
jppuerto <jeremy.puertolas@gmail.com>
2021-06-17 11:56:43 +0200

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,59 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "entrée pyramide":
+            case "level1":return tiles.createTilemap(hex`1000100008080808080808080808080808080808080808080808080808080808080808080808080808080806080808080808080808080808080806060608080808080808080808080806060606060808080808080808080806060606060606080808080808080806060606060606060608080808080806060606060606060606060808080806060606060606060606060606080806060606060606060606060606060608060606060606030504060606060606060606060606060301020606060606060607070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.dungeon.stairNorth,sprites.dungeon.floorDark5,sprites.dungeon.floorDark1,sprites.dungeon.floorLightMoss,sprites.dungeon.doorOpenNorth,myTiles.tile4,sprites.castle.tilePath5,myTiles.tile3], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "myTile":
+            case "tile1":return tile1;
+            case "myTile0":
+            case "tile2":return tile2;
+            case "brique pyramide":
+            case "tile4":return tile4;
+            case "myTile1":
+            case "tile3":return tile3;
+            case "eau":
+            case "tile5":return tile5;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
